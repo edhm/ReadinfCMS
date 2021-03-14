dfa4de24da7382de5527a3801d892a914d5c3b55
Edgar Huerta <ed.huerta.m@gmail.com>
2021-03-14 15:53:28 -0500

    Initial ReadingCMS

---
+++ src/test/java/com/ehm/ReadingCMS/ReadingCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehm.ReadingCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
